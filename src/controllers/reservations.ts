7808b2c9751287158a058b8444c718b2001a98e6
Oluwatomi Ajaka <oluwatomi.ajaka@veendhq.com>
2025-10-30 02:11:19 +0100

    final fix-hopefully

--- src/controllers/reservations.ts
+++ src/controllers/reservations.ts
@@ -33,7 +33,7 @@ export async function sendMail(
 
 		// Create HTML email with embedded image
 		const messageParts = [
-			`From: "Tope and Funmbi" <${EMAIL_USER}>`,
+			`From: '"Tope and Funmbi" <${EMAIL_USER}>'`,
 			`To: ${to}`,
 			"Subject: Invitation to our celebration",
 			"MIME-Version: 1.0",
